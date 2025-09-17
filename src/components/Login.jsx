c4991e5cb8a86fdb54ef22175a9ee06d9105eef9
luis505050 <luisferna267@gmail.com>
2025-09-17 08:14:10 -0500

    Login ajustado o

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { loginUser } from "../api/api"; // importamos nuestra API
-import "../templates/styles/login.css"
+import { Eye, EyeOff } from "lucide-react"; // importamos iconos
+import "../templates/styles/login.css";
 
 export default function Login() {
   const [form, setForm] = useState({ email: "", password: "" });
   const [loading, setLoading] = useState(false);
+  const [showPassword, setShowPassword] = useState(false); // estado para ver/ocultar
   const navigate = useNavigate();
 
   const handleChange = (e) => {
@@ -17,7 +19,7 @@ export default function Login() {
     setLoading(true);
 
     try {
-      const data = await loginUser(form); // 👈 aquí usamos api.js
+      const data = await loginUser(form); //aquí usamos api.js
 
       // Guardar token y user
       localStorage.setItem("token", data.token);
@@ -44,38 +46,65 @@ export default function Login() {
   };
 
   return (
-    <div className="d-flex align-items-center justify-content-center vh-100  principal-content">
-      <div className="card shadow p-4" style={{ width: "100%", maxWidth: "400px" }}>
+    <div className="d-flex align-items-center justify-content-center vh-100 principal-content">
+      {/* Botón Asistencia */}
+      <button
+        onClick={() => (window.location.href = "/")}
+        className="btn btn-primary btn-volver"
+      >
+        Volver
+      </button>
+      <div
+        className="card shadow p-4"
+        style={{ width: "100%", maxWidth: "400px" }}
+      >
         <h2 className="text-center text-primary mb-4">Sistema de Asistencia</h2>
-        <div className="d-flex justify-content-center ">   
+        <div className="d-flex justify-content-center">
           <img className="img-user-login" src="/Sample_User_Icon.png" alt="" />
         </div>
         <form onSubmit={handleSubmit}>
-          <div className="mb-3">
-            <label className="form-label">Correo</label>
-            <input
-              type="email"
-              name="email"
-              value={form.email}
-              onChange={handleChange}
-              className="form-control"
-              required
-              autoComplete="username"
-            />
+          <div className="wrapper">
+            <div className="mb-3">
+              <label className="form-label">Correo</label>
+              <input
+                type="email"
+                name="email"
+                value={form.email}
+                onChange={handleChange}
+                className="form-control"
+                required
+                autoComplete="username"
+              />
+            </div>
           </div>
+
+          {/* Campo de contraseña con ojo */}
           <div className="mb-3">
             <label className="form-label">Contraseña</label>
-            <input
-              type="password"
-              name="password"
-              value={form.password}
-              onChange={handleChange}
-              className="form-control"
-              required
-              autoComplete="current-password"
-            />
+            <div className="wrapper">
+              <input
+                type={showPassword ? "text" : "password"}
+                name="password"
+                value={form.password}
+                onChange={handleChange}
+                required
+                autoComplete="current-password"
+              />
+              <button
+                type="button"
+                className="password-toggle"
+                onClick={() => setShowPassword(!showPassword)}
+              >
+                {showPassword ? <EyeOff size={20} /> : <Eye size={20} />}
+              </button>
+            </div>
           </div>
-          <button type="submit" className="btn btn-primary w-100" disabled={loading}>
+
+          <button
+            type="submit"
+            className="btn btn-primary  btn-pass"
+            disabled={loading}
+          >
             {loading ? "Ingresando..." : "Iniciar Sesión"}
           </button>
         </form>
